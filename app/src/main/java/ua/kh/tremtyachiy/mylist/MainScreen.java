38b15df49a2fd8425ab0455e40f80b216f95ad06
mrsnoopy86 <tremtyachiy@gmail.com>
2015-06-18 18:04:01 +0300

    cleared

--- app/src/main/java/ua/kh/tremtyachiy/mylist/MainScreen.java
+++ app/src/main/java/ua/kh/tremtyachiy/mylist/MainScreen.java
@@ -1,10 +1,8 @@
 package ua.kh.tremtyachiy.mylist;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.Button;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -12,14 +10,12 @@ import cn.pedant.SweetAlert.SweetAlertDialog;
  * Created by User on 18.06.2015.
  */
 public class MainScreen extends ActionBarActivity {
-    Button btn;
     SweetAlertDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen);
-        btn = (Button) findViewById(R.id.buttonAdd);
     }
 
     public void onClickMe(View view){
