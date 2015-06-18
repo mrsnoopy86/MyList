1b073a1710adacd36b347dc9f2b91cd35ec0b617
mrsnoopy86 <tremtyachiy@gmail.com>
2015-06-18 17:17:14 +0300

    create main screen

---
+++ app/src/main/java/ua/kh/tremtyachiy/mylist/MainScreen.java
@@ -0,0 +1,16 @@
+package ua.kh.tremtyachiy.mylist;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by User on 18.06.2015.
+ */
+public class MainScreen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainscreen);
+    }
+}
