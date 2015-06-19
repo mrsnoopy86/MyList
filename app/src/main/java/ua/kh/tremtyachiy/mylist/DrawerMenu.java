dba608a241a1522f31645df0f0f6f08c7f66029c
mrsnoopy86 <tremtyachiy@gmail.com>
2015-06-19 12:16:41 +0300

    add tabHost

---
+++ app/src/main/java/ua/kh/tremtyachiy/mylist/DrawerMenu.java
@@ -0,0 +1,62 @@
+package ua.kh.tremtyachiy.mylist;
+
+import android.app.Activity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+/**
+ * Created by User on 19.06.2015.
+ */
+public class DrawerMenu {
+    private Drawer drawerMenu;
+
+    public Drawer getDrawerMenu() {
+        return drawerMenu;
+    }
+
+    public void initDrawerMenu(Activity activity, Toolbar toolbar){
+        drawerMenu = new DrawerBuilder()
+                .withSliderBackgroundColor(activity.getResources().getColor(R.color.blue_main))
+                .withToolbar(toolbar)
+                .withDisplayBelowToolbar(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withActivity(activity)
+                .addDrawerItems(
+                        new SecondaryDrawerItem()
+                                .withIcon(R.drawable.ic_settings_white_24dp)
+                                .withName(R.string.nav_menu_item1)
+                                .withIdentifier(1),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item2)
+                                .withIcon(R.drawable.ic_announcement_white_24dp)
+                                .withIdentifier(2),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item3)
+                                .withIcon(R.drawable.ic_supervisor_account_white_24dp)
+                                .withIdentifier(3),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item4)
+                                .withIcon(R.drawable.ic_live_help_white_24dp)
+                                .withIdentifier(4),
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem()
+                                .withName(R.string.nav_menu_item5)
+                                .withIcon(R.drawable.ic_exit_to_app_white_24dp)
+                                .withIdentifier(5))
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+                        return true;
+                    }
+                })
+                .build();
+    }
+}
--- app/src/main/java/ua/kh/tremtyachiy/mylist/MainScreen.java
+++ app/src/main/java/ua/kh/tremtyachiy/mylist/MainScreen.java
@@ -1,5 +1,6 @@
 package ua.kh.tremtyachiy.mylist;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,12 +15,14 @@ import cn.pedant.SweetAlert.SweetAlertDialog;
 public class MainScreen extends ActionBarActivity {
     SweetAlertDialog pDialog;
     Toolbar toolbar;
+    private DrawerMenu drawerMyMenu = new DrawerMenu();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainscreen);
         initElements();
+        drawerMyMenu.initDrawerMenu(this, toolbar);
     }
 
     private void initElements() {
@@ -28,6 +31,7 @@ public class MainScreen extends ActionBarActivity {
          */
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(Color.WHITE);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -39,7 +43,7 @@ public class MainScreen extends ActionBarActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                drawerMyMenu.getDrawerMenu().openDrawer();
             }
         });
     }
